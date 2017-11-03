eaf1a3b4fb5e96502de45617dda4d3b1de29c6f4
leimiaomiao <leimiaomiao@126.com>
2017-11-03 21:15:42 +0800

    modify calc max rel and min rel methods

--- util/MaxRelAlgorithm.py
+++ util/MaxRelAlgorithm.py
@@ -9,8 +9,14 @@ class MaxRelAlgorithm(object):
 
         i = 0
         self.individual = Individual(self, i, workflow, lambda_list)
-        self.individual.schedule()
-        self.rel = self.individual.calc_rel()
+
+        self.rel = 0
+        for pos in range(0, 15):
+            self.individual.individual_task_list = self.init_task_list_order_pos_at(pos)
+            self.individual.schedule()
+
+            if self.rel < self.individual.calc_rel():
+                self.rel = self.individual.calc_rel()
 
     def init_task_list_order_pos(self):
         # 任务排序初始化
@@ -22,5 +28,18 @@ class MaxRelAlgorithm(object):
             individual_task = IndividualTask()
             individual_task.task = task
             individual_task.exec_pos = 4
+            individual_task_list.append(individual_task)
+        return individual_task_list
+
+    def init_task_list_order_pos_at(self, pos):
+        # 任务排序初始化
+        task_list_ordered = self.workflow.task_list
+
+        # 任务位置初始化
+        individual_task_list = list()
+        for task in task_list_ordered:
+            individual_task = IndividualTask()
+            individual_task.task = task
+            individual_task.exec_pos = pos
             individual_task_list.append(individual_task)
         return individual_task_list
--- util/MinRelAlgorithm.py
+++ util/MinRelAlgorithm.py
@@ -10,8 +10,16 @@ class MinRelAlgorithm(object):
 
         i = 0
         self.individual = Individual(self, i, workflow, lambda_list)
-        self.individual.schedule()
-        self.rel = self.individual.calc_rel()
+        self.rel = None
+        for pos in range(0, 15):
+            self.individual.individual_task_list = self.init_task_list_order_pos_at(pos)
+            self.individual.schedule()
+
+            if self.rel is None:
+                self.rel = self.individual.calc_rel()
+
+            elif self.rel > self.individual.calc_rel():
+                self.rel = self.individual.calc_rel()
 
     def init_task_list_order_pos(self):
         # 任务排序初始化
@@ -23,5 +31,18 @@ class MinRelAlgorithm(object):
             individual_task = IndividualTask()
             individual_task.task = task
             individual_task.exec_pos = 10
+            individual_task_list.append(individual_task)
+        return individual_task_list
+
+    def init_task_list_order_pos_at(self, pos):
+        # 任务排序初始化
+        task_list_ordered = self.workflow.task_list
+
+        # 任务位置初始化
+        individual_task_list = list()
+        for task in task_list_ordered:
+            individual_task = IndividualTask()
+            individual_task.task = task
+            individual_task.exec_pos = pos
             individual_task_list.append(individual_task)
         return individual_task_list
